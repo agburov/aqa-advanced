2822e65a780b61ff0c2ac2ce59113c5414f2143c
Arthur Burov <art.burov@ukr.net>
2025-06-05 20:40:14 +0300

    JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let myString = 'Hello JavaScript!';
+let myNumber = 55;
+let myBoolean = true;
+let myNull = null;
+let myUndefined;
+
+console.log('String:', myString);
+console.log('Number:', myNumber);
+console.log('Boolean:', myBoolean);
+console.log('Null:', myNull);
+console.log('Undefined:', myUndefined);
---
+++ jsBasics/task3.js
@@ -0,0 +1,6 @@
+let age = 15;
+
+let isAdult = age >= 18;
+
+console.log('Age:', age);
+console.log('Is Adult:', isAdult);
